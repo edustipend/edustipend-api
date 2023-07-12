3c88f29b63870e22a2bf81436ad8a0dd6eee1069
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-07-12 14:58:18 +0100

    Add clearer json responses

--- controller/UserController.js
+++ controller/UserController.js
@@ -3,14 +3,21 @@ const catchAsyncError = require("../middleware/catchAsyncError");
 const models = require("../models");
 
 exports.requestStipend = catchAsyncError(async (req, res) => {
-  const payload = req.body;
+  try {
+    const payload = req.body;
 
-  await models.laptopRequest.create({
-    ...payload
-  });
+    await models.laptopRequest.create({
+      ...payload
+    });
 
-  return res.status(201).json({
-    success: true,
-    message: "Request successfully sent"
-  });
+    return res.status(201).json({
+      success: true,
+      message: "Request successfully sent"
+    });
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      message: "Request not completed"
+    })
+  }
 });
