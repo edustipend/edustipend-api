b3d84b82bcd8ec6d3e28dae1a1b5d244156e2ceb
Eloghosa Osagie <33532766+Elozzy@users.noreply.github.com>
2023-07-24 03:53:49 +0100

    Revert "Feature/reset password"

--- services/Authentication.js
+++ services/Authentication.js
@@ -1,18 +1,9 @@
 const bcrypt = require("bcryptjs");
 const User = require("./User");
 const Token = require("./Token");
-const {
-  randomSixDigits,
-  validateEmail,
-  cryptWords,
-  hasEmptySpace
-} = require("../utils/helper");
+const { randomSixDigits, validateEmail } = require("../utils/helper");
 const ErrorHandler = require("../utils/ErrorHandler");
 const models = require("../models");
-const { where } = require("sequelize");
-const mail = require("./index");
-// const { validateRegisterData } = require("../validation/UserValidation");
-const { error } = require("@hapi/joi/lib/annotate");
 
 class Authentication {
   /**
@@ -66,8 +57,6 @@ class Authentication {
     return { name, email, code };
   }
 
-
-
   /**
    * @description Account Verification
    * @param {string} email
@@ -107,84 +96,25 @@ class Authentication {
     };
   }
 
-  static async passwordReset(data) {
-    const { name, email } = data;
-    if (!email) {
-      throw new ErrorHandler("Email is required", 400);
-    }
-    const oldUserEmail = await models.user.findOne({ where: { email } });
-    if (oldUserEmail === null) {
-      throw new ErrorHandler("User not found", 404);
-    }
-    const code = randomSixDigits();
-    await Token.genCode(email, code);
-    return { name, email, code };
-  }
-
-
-  static async passwordUpdate({
-    email,
-    verificationCode,
-    password,
-    confirmPassword
-  }) {
-    if (!email) {
-      throw new ErrorHandler("Email is required", 400);
-    }
-    if (!verificationCode) {
-      throw new ErrorHandler("Verification code is required", 400);
-    }
-    const token = await Token.validateCode(verificationCode);
-    const setVerify = models.user.update(
-      { code: verificationCode },
-      { where: { email } }
-    );
-    const userInstance = User.findOne(email);
-    const response = await Promise.all([setVerify, userInstance]);
-    if (response[1] === null) {
-      throw new ErrorHandler("User does not exist", 404);
-    }
-    if (!password) {
-      throw new ErrorHandler("password is required", 400);
-    }
-    if (password !== confirmPassword) {
-      throw new ErrorHandler("passwords does not match", 400);
-    }
-    // User.update({ password: 'new_password' }, { where: { email: 'user@example.com' } });
-
-    const hashedPassword = await this.hashpassword(password);
-
-    const newPassword = await models.user.update(
-      { password: hashedPassword },
-      { where: { email: email } }
-    );
-
-    Token.deleteEmail(token.email);
-    const jwtToken = response[1].generateJwtToken();
-
-    return {
-      token: jwtToken,
-      name: response[1].name,
-      email: response[1].email
-    };
-  }
-
   /**
-@description Login a user
-@param {object} email
-@param {string} password
+   * @description Login a user
+   * @param {object} email
+   * @param {string} password
    */
+
   static async loginUser({ email, password }) {
     const newUser = await User.findOne(email);
     let data;
     if (newUser === null) {
       throw new ErrorHandler("Invalid credential", 401);
     }
+
     const checkPassword = await newUser.comparePassword(password);
     if (!checkPassword) {
       throw new ErrorHandler("Invalid credential", 401);
     }
     data = newUser.generateJwtToken();
+
     return {
       success: true,
       message: "Sign in successful",
