d741bb31fd09524dad6ede5f973a0b521d344d8b
linda <lindaochem@gmail.com>
2023-07-24 15:04:18 +0100

    edited dummyData

--- test/dummyData.js
+++ test/dummyData.js
@@ -1,43 +1,26 @@
 const faker = require("faker");
 
 const registerUser = {
-  name: "linda",
-  email: "linda@gmail.com",
+  name: "eloghosa",
+  email: "tes@gmail.com",
   password: "string123456",
   dateOfBirth: "1990-09-02",
-  gender: "female",
+  gender: "male",
   stateOfOrigin: "lagos",
   howDidYouHearAboutUs: "facebook"
 };
 
 const badRegisterUserRequest = {
-  name: "linda",
-  email: "linda@gmail.com",
+  name: "eloghosa",
+  email: "test@gmail.com",
   password: "string123456",
 
   gender: "male",
   howDidYouHearAboutUs: "facebook"
 };
 
-const passwordReset = {
-  email: "linda@gmail.com",
-  name: "linda"
-};
-
-const badPasswordReset = {
-  email: "linda@gmail.com"
-};
-
-const passwordVerify = {
-  email: "linda@gmail.com",
-  password: "string123456",
-  verificationCode: "ABCD1234"
-};
 
 module.exports = {
   registerUser,
-  badRegisterUserRequest,
-  passwordReset,
-  badPasswordReset,
-  passwordVerify
+  badRegisterUserRequest
 };
