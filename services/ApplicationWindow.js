2880c266790b8f78b590093191ffe4d97df01a08
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-07-30 19:56:51 +0100

    create ApplicationWindow service

---
+++ services/ApplicationWindow.js
@@ -0,0 +1,20 @@
+const models = require("../models");
+const ErrorHandler = require("../utils/ErrorHandler");
+
+class ApplicationWindow {
+  /**
+   * @description create new application window
+   * @param {object} data
+   */
+
+  static async create(data) {
+    // Set isClosedByAdmin to true if it is truthy, otherwise set it to false (default).
+    const isClosedByAdmin = !!data.isClosedByAdmin
+
+    return await models.applicationWindow.create({
+      ...data,
+      isClosedByAdmin
+    })
+
+  }
+}
