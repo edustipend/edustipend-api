3acf57a96d6a9404e79b4fa664a67542018ef075
Chiemezuo <chiemezuoakujobi@gmail.com>
2023-08-07 23:46:28 +0100

    changes to waitlist test

--- test/integration/WaitlistController.test.js
+++ test/integration/WaitlistController.test.js
@@ -5,8 +5,10 @@ const expect = chai.expect;
 const server = require("../../server");
 const models = require("../../models");
 
-const {} = require("../dummyData");
+const { registerUser } = require("../dummyData");
 const { declutter } = require("../../database/migration/test");
+
+let res;
 
 describe("Tests for waitlist", function () {
   this.beforeAll(async function () {
@@ -15,21 +17,62 @@ describe("Tests for waitlist", function () {
   });
 
   describe("Tests for joining waitlist", function () {
-    /**
-     * @todo add beforeAll function
-     */
+    this.beforeAll(async function () {
+      this.timeout(0);
 
-    it.skip("should check that a success status code is returned when a user adds their email to waitlist", async function () {});
-    it.skip("should check that the email reflects in the database", async function () {});
+      res = await chai.request(server).post("/v1/waitlist/join-waitlist").send({
+        email: registerUser.email,
+        name: registerUser.name,
+        howDidYouHearAboutUs: registerUser.howDidYouHearAboutUs
+      });
+    });
+
+    it("should check that a success status code is returned when a user adds their email to waitlist", async function () {
+      expect(res).to.have.status(201);
+    });
+    it("should check that the email reflects in the database", async function () {
+      let userInDb = await models.waitlist.findOne({
+        where: {
+          email: registerUser.email
+        }
+      });
+      expect(userInDb.email).to.equal(registerUser.email);
+    });
+  });
+
+  describe("Incomplete waitlist data should not go through", function () {
+    this.beforeAll(async function () {
+      this.timeout(0);
+
+      res = await chai.request(server).post("/v1/waitlist/join-waitlist").send({
+        name: registerUser.name,
+        howDidYouHearAboutUs: registerUser.howDidYouHearAboutUs
+      });
+    });
+
+    it("should fail when data is incomplete", async function () {
+      expect(res).to.have.status(400);
+    });
   });
 
   describe("Tests for notifying waitlist", function () {
-    /**
-     * @todo add beforeAll function
-     */
+    this.beforeAll(async function () {
+      this.timeout(0);
 
-    it.skip("should check that a success status code is returned when admin notifies waitlist", async function () {});
+      res = await chai.request(server).post("/v1/admin/notify-waitlist");
+    });
+
+    it("should check that a success status code is returned when admin notifies waitlist", async function () {
+      expect(res).to.have.status(201);
+    });
+    it("should check that when the waitlist is notified, the hasBeenNotified field becomes true", async function () {
+      let userInDb = await models.waitlist.findOne({
+        where: {
+          email: registerUser.email
+        }
+      });
+      expect(userInDb.hasBeenNotified).to.equal(true);
+    });
     it.skip("should check that only an admin can notify the waitlist", async function () {});
-    it.skip("should check that when the waitlist is notified, the hasBeenNotified field becomes true", async function () {});
   });
 });
