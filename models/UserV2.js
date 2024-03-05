a19e68218146cfbe33452766d4b60369398196ac
Edustipend Root <edustipenddev@gmail.com>
2024-03-04 17:40:36 -0800

    feat: add int'l women's day to referal sources

--- models/UserV2.js
+++ models/UserV2.js
@@ -31,7 +31,17 @@ const UserSchema = new Schema(
     },
     howDidYouHearAboutUs: {
       type: String,
-      enum: ["facebook", "twitter", "instagram", "other", "linkedin", "int'l women's day", "google search", "community", "x"],
+      enum: [
+        "facebook",
+        "twitter",
+        "instagram",
+        "other",
+        "linkedin",
+        "int'l women's day",
+        "google search",
+        "community",
+        "x"
+      ],
       required: false
     },
     isAdmin: {
