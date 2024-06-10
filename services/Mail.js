ebd7318b32830edcb9148926904c8af822f959c7
Chiemezuo <chiemezuoakujobi@gmail.com>
2024-06-11 00:38:05 +0100

    add thank you email for donors

--- services/Mail.js
+++ services/Mail.js
@@ -263,6 +263,25 @@ class Mail {
       }
     );
   }
+
+  /**
+   * @description Send appreciation email after successful donation
+   */
+  static sendThankYouForDonation(email, amount) {
+    this._sendEmail(
+      {
+        email,
+        subject: "Thank You For Your Support",
+        template: "thank-you-for-donating",
+        params: {
+          amount
+        }
+      },
+      (err, data) => {
+        if (err) console.log(err);
+      }
+    );
+  }
 }
 
 module.exports = Mail;
